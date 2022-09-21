66c265e76504e312c02f0add2c5ff229465e270f
ArtemSmirnovHSE <ar.smirnov2015@mail.ru>
2022-09-21 12:17:33 +0300

    Added blank info display functions

--- main.py
+++ main.py
@@ -20,37 +20,78 @@ def readContactsFile(filename):
 
 contactList = readContactsFile('contacts.txt')
 
-def findByPhone(contactlist, phone_to_find):
+def findByPhone(contactlist:Contacts, phone_to_find:str) -> Contacts:
     for contact in contactlist:
         contact_phone = contact.getPhone()
         if phone_to_find==contact_phone: return contact
     
     raise Exception('Телефон не найден')
 
-def findByEmail(contactlist,email_to_find):
+def findByEmail(contactlist:Contacts, email_to_find:str) -> Contacts:
     for contact in contactlist:
         contact_email = contact.getEmail()
         if email_to_find==contact_email: return contact
     
     raise Exception('Почта не найдена')
         
-def findByFullname(contactlist, fullname_to_find):
+def findByFullname(contactlist:Contacts, fullname_to_find:str) -> Contacts:
     for contact in contactlist:
         contact_fullname = contact.getFullName()
         if fullname_to_find==contact_fullname: return contact
     
     raise Exception('Контакт не найден')
 
-def findByMiddleName(contactlist:Contacts, middlename_to_find: str) -> Contacts:
+def findByMiddleName(contactlist:Contacts, middlename_to_find:str) -> Contacts:
     for contact in contactlist:
         contact_middlename = contact.getMiddleName()
         if middlename_to_find==contact_middlename: return contact
     
     raise Exception('Контакт не найден')
     
+def findByFirstName(contactlist:Contacts, firstname_to_find:str) -> Contacts:
+    for contact in contactlist:
+        contact_fistname = contact.getFirstName()
+        if firstname_to_find==contact_fistname: return contact
+    
+    raise Exception('Контакт не найден')
+
+def findByLastName(contactlist:Contacts, lastname_to_find:str) -> Contacts:
+    for contact in contactlist:
+        contact_lastname = contact.getLastName()
+        if lastname_to_find==contact_lastname: return contact
+    
+    raise Exception('Контакт')
+
+def BlankPhone(contactlist:Contacts) -> list:
+    blank_phone_contacts = []
+    for contact in contactlist:
+        if len(contact.getPhone())==0: blank_phone_contacts.append(contact)
+    return blank_phone_contacts
+
+def BlankEmail(contactlist:Contacts) -> list:
+    blank_email_contacts  = []
+    for contact in contactlist:
+        if len(contact.getEmail())==0: blank_email_contacts.append(contact)
+    return blank_email_contacts
+
+def BlankAll(contactlist:Contacts) -> list:
+    blank_phone_contacts = BlankEmail(contactlist)
+    blank_email_contacts = BlankPhone(contactlist)
+    blank_all = list(set(blank_email_contacts+blank_phone_contacts))
+    return blank_all
 # print(findByPhone(contactList, '+79966969669'))
 # print(findByEmail(contactList, 'kolya@gmail.com'))
-print(findByMiddleName(contactList, 'Сергеевич'))
+# print(findByMiddleName(contactList, 'Сергеевич'))
+# for phone in BlankPhone(contactlist=contactList):
+#     print(phone) 
+    
+for phone in BlankEmail(contactlist=contactList):
+    print(phone) 
+# for phone in BlankAll(contactlist=contactList):
+#     print(phone) 
+
+
+
         
 
     
